874f2d59f2ce40cbe3cd4b75e15d5924655969a8
MRITUNJAY-SINGHH <mritunjays447@gmail.com>
2024-01-07 23:37:45 +0530

    git commit -m "header component"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import { FaSearch } from 'react-icons/fa';
 import Logo from '../../assets/logo.svg';
-import Compare from '../../assets/icon-compare.svg';
-import Cart from '../../assets/icon-cart.svg';
-import Favorite from '../../assets/icon-favorite.svg';
+import { IoGitCompare } from 'react-icons/io5';
+import { FaCartArrowDown } from 'react-icons/fa';
+import { MdFavoriteBorder } from 'react-icons/md';
 import './Header.css';
 import AutocompleteComponents from '../../Common/Autocomplete';
 import DarkNightMode from '../../Common/DarkNightMode';
@@ -35,33 +35,42 @@ const Header = () => {
             </div>
 
             <div className='options w-[35%] flex justify-around items-center'>
-               <div className='flex justify-center item-center space-x-2 relative cursor-pointer'>
-                  <img src={Compare} alt='logo' className='' />
-                  <span className='text-sm font-semibold dark:text-white text-black'>
-                     Compare
-                  </span>
-                  <span className='absolute top-[-8px] left-1 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-5 w-5 flex items-center justify-center text-xs'>
-                     6
-                  </span>
+               <div className='flex items-center justify-center'>
+                  <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                     <IoGitCompare className='h-7 w-7' />
+                     <span className='text-sm font-semibold dark:text-white text-black'>
+                        Compare
+                     </span>
+                     <span className='absolute top-[-20px] left-2 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                        6
+                     </span>
+                  </div>
                </div>
-               <div className='flex justify-center item-center space-x-2 relative cursor-pointer'>
-                  <img src={Favorite} alt='logo' className='w-full' />
-                  <span className='text-sm font-semibold dark:text-white text-black'>
-                     Wishlist
-                  </span>
-                  <span className='absolute top-[-8px] left-1 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-5 w-5 flex items-center justify-center text-xs'>
-                     6
-                  </span>
+
+               <div className='flex items-center justify-center'>
+                  <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                     <MdFavoriteBorder className='h-7 w-7' />
+                     <span className='text-sm font-semibold dark:text-white text-black'>
+                        Wishlist
+                     </span>
+                     <span className='absolute top-[-20px] left-0 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                        6
+                     </span>
+                  </div>
                </div>
-               <div className='flex justify-center item-center space-x-2 relative cursor-pointer'>
-                  <img src={Cart} alt='logo' />
-                  <span className='text-sm font-semibold dark:text-white text-black'>
-                     Cart
-                  </span>
-                  <span className='absolute top-[-8px] left-1 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-5 w-5 flex items-center justify-center text-xs'>
-                     6
-                  </span>
+
+               <div className='flex items-center justify-center'>
+                  <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                     <FaCartArrowDown className='h-7 w-7 flex justify-center items-center' />
+                     <span className='text-sm font-semibold dark:text-white text-black'>
+                        Cart
+                     </span>
+                     <span className='absolute top-[-20px] left-1 bg-[#29a56c] dark:bg-white text-black dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                        6
+                     </span>
+                  </div>
                </div>
+
                <div className='flex justify-center item-center  relative  cursor-pointer'>
                   <DropdownMenu />
                </div>
