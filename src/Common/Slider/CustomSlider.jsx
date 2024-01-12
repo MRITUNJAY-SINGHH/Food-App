ded3ce149b2816d06eda777bfcb3859c63134b53
MRITUNJAY-SINGHH <mritunjays447@gmail.com>
2024-01-12 21:52:53 +0530

    git commit -m "improve slider"

--- src/Common/Slider/CustomSlider.jsx
+++ src/Common/Slider/CustomSlider.jsx
@@ -4,19 +4,19 @@ import 'react-multi-carousel/lib/styles.css';
 import 'react-multi-carousel/lib/styles.css';
 import category from '../../assets/cat-1.png';
 import category2 from '../../assets/cat-2.png';
-import category3 from '../../assets/cat-1.png';
-import category4 from '../../assets/cat-1.png';
-import category5 from '../../assets/cat-15.png';
+import category3 from '../../assets/cat-3.png';
+import category4 from '../../assets/cat-4.png';
+import category5 from '../../assets/cat-5.png';
 import category6 from '../../assets/cat-15.png';
-import category7 from '../../assets/cat-11.png';
-import category8 from '../../assets/cat-9.png';
+import category7 from '../../assets/cat-13.png';
+import category8 from '../../assets/cat-3.png';
 import category9 from '../../assets/cat-9.png';
-import category10 from '../../assets/cat-11.png';
 import category11 from '../../assets/cat-11.png';
 import category12 from '../../assets/cat-12.png';
 import category13 from '../../assets/cat-13.png';
 import category14 from '../../assets/cat-14.png';
 import category15 from '../../assets/cat-15.png';
+import './Slider.css';
 import { IoIosArrowRoundForward, IoIosArrowRoundBack } from 'react-icons/io';
 import './Slider.css';
 
@@ -48,114 +48,119 @@ export default function HomeSlider() {
    const data = [
       {
          id: 1,
-         name: 'Category 1',
+         name: 'Vegetables',
          image: category,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#F2FCE4',
       },
       {
          id: 2,
-         name: 'Category 2',
+         name: 'Strawberry',
          image: category2,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#F2FCE4',
       },
       {
          id: 3,
-         name: 'Category 3',
+         name: 'Snakes',
          image: category3,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFFCEB',
       },
       {
          id: 4,
-         name: 'Category 4',
+         name: 'Black pulm',
          image: category4,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FEEFEA',
       },
       {
          id: 5,
-         name: 'Category 5',
+         name: 'Custard Apple',
          image: category5,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFF3EB',
       },
       {
          id: 6,
-         name: 'Category 6',
+         name: 'Headphones',
          image: category6,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFF3FF',
       },
       {
          id: 7,
-         name: 'Category 7',
+         name: 'Cake & Pastry',
          image: category7,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#F2FCE4',
       },
       {
          id: 8,
-         name: 'Category 8',
+         name: 'Snakes',
          image: category8,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FEEFEA',
       },
       {
          id: 9,
-         name: 'Category 9',
+         name: 'Red Apple',
          image: category9,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
-      },
-      {
-         id: 10,
-         name: 'Category 10',
-         image: category10,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFFCEB',
       },
       {
          id: 11,
-         name: 'Category 11',
+         name: 'Peach',
          image: category11,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFF3EB',
       },
       {
          id: 12,
-         name: 'Category 12',
+         name: 'Organic Kiwi',
          image: category12,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#F2FCE4',
       },
       {
          id: 13,
-         name: 'Category 13',
+         name: 'Cake & Pastry',
          image: category13,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FEEFEA',
       },
       {
          id: 14,
-         name: 'Category 14',
+         name: 'Coffee & Tea',
          image: category14,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FFFCEB',
       },
       {
          id: 15,
-         name: 'Category 15',
+         name: 'Headphones',
          image: category15,
-         item: 'Item 264',
-         backgroundColor: '#ECFFEC',
+         item: `Item ${Math.floor(Math.random() * 1000)}`,
+         backgroundColor: '#FEEFEA',
       },
    ];
 
    return (
-      <section className='p-6 mt-6'>
-         <div className='w-full rounded-2xl overflow-hidden relative'>
+      <div className='p-6 mt-6'>
+         <div className='w-full rounded-2xl overflow-hidden'>
+            <div className='flex items-center paragraph'>
+               <div className='px-2'>
+                  <h1 className='commonHeading'>Featured Categories</h1>
+               </div>
+               <div className='flex items-center justify-center ml-10  cursor-pointer paragraph'>
+                  <p>Cake & Milk</p>
+                  <p>Cake & Milk</p>
+                  <p>Cake & Milk</p>
+                  <p>Cake & Milk</p>
+               </div>
+            </div>
+
             <Carousel
                responsive={responsive}
                showDots={false}
@@ -163,12 +168,8 @@ export default function HomeSlider() {
                autoPlay
                autoPlaySpeed={3000}
                transitionDuration={500}
-               // removeArrowOnDeviceType={['tablet', 'mobile']}
-               customButtonGroup={<></>}
-               customLeftArrow={<CustomLeftArrow />}
-               customRightArrow={<CustomRightArrow />}
                beforeChange={customBeforeChange}
-               className='pt-[5rem] px-4 pb-[2rem]'
+               className='pt-[5rem] px-4 pb-[2rem] relative'
             >
                {data.map((item, id) => (
                   <div
@@ -176,37 +177,24 @@ export default function HomeSlider() {
                      className='item mx-2'
                      style={{
                         backgroundColor: item.backgroundColor,
+                        minHeight: '180px',
                      }}
                   >
                      <img src={item.image} alt={item.name} />
                      <h3 className='text-[#253d4e] font-bold text-md'>
                         {item.name}
                      </h3>
-                     <h4>{item.item}</h4>
+                     <h4 className='text-sm'>{item.item}</h4>
                   </div>
                ))}
             </Carousel>
+            <button className='bottom-10 left-1/2 transform -translate-x-1/2 bg-blue-500 px-4 py-2 text-white rounded-md'>
+               <IoIosArrowRoundForward />
+            </button>
+            <button className='bottom-10 left-1/2 transform -translate-x-1/2 bg-blue-500 px-4 py-2 text-white rounded-md'>
+               <IoIosArrowRoundBack />
+            </button>
          </div>
-      </section>
+      </div>
    );
 }
-
-const CustomLeftArrow = ({ onClick }) => (
-   <button
-      type='button'
-      onClick={onClick}
-      className='custom-arrowSlider custom-prevSlider'
-   >
-      <IoIosArrowRoundBack size={30} />
-   </button>
-);
-
-const CustomRightArrow = ({ onClick }) => (
-   <button
-      type='button'
-      onClick={onClick}
-      className='custom-arrowSlider custom-nextSlider'
-   >
-      <IoIosArrowRoundForward size={30} />
-   </button>
-);
--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -4,6 +4,7 @@ import 'react-multi-carousel/lib/styles.css';
 import sliderImage1 from '../../assets/slider-1.png';
 import sliderImage2 from '../../assets/slider-2.png';
 import { IoIosArrowRoundForward, IoIosArrowRoundBack } from 'react-icons/io';
+import { Transition } from '@headlessui/react';
 import '../../index.css';
 import 'react-multi-carousel/lib/styles.css';
 
@@ -29,46 +30,56 @@ export default function HomeSlider() {
    return (
       <section className='p-6'>
          <div className='w-full rounded-2xl overflow-hidden relative'>
-            <Carousel
-               responsive={responsive}
-               showDots={true}
-               infinite
-               autoPlay
-               autoPlaySpeed={3000}
-               transitionDuration={500}
-               // removeArrowOnDeviceType={['tablet', 'mobile']}
-               customButtonGroup={<></>} // This is to remove default button groups
-               customLeftArrow={<CustomLeftArrow />}
-               customRightArrow={<CustomRightArrow />}
-               dotListClass='custom-dot-list'
+            <Transition
+               show={true}
+               enter='transition-opacity duration-500'
+               enterFrom='opacity-0'
+               enterTo='opacity-100'
+               leave='transition-opacity duration-500'
+               leaveFrom='opacity-100'
+               leaveTo='opacity-0'
             >
-               <div>
-                  <img src={sliderImage1} alt='slider' className='w-full' />
-                  <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
-                     <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                        Don’t miss amazing
-                        <br />
-                        grocery deals
-                     </h2>
-                     <p className='text-3xl text-gray-500 font-medium mb-12'>
-                        Save up to 50% off on your first order
-                     </p>
+               <Carousel
+                  responsive={responsive}
+                  showDots={true}
+                  infinite
+                  autoPlay
+                  autoPlaySpeed={3000}
+                  transitionDuration={500}
+                  // removeArrowOnDeviceType={['tablet', 'mobile']}
+                  customButtonGroup={<></>} // This is to remove default button groups
+                  customLeftArrow={<CustomLeftArrow />}
+                  customRightArrow={<CustomRightArrow />}
+                  dotListClass='custom-dot-list'
+               >
+                  <div>
+                     <img src={sliderImage1} alt='slider' className='w-full' />
+                     <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
+                        <h2 className='text-7xl text-gray-800 font-bold mb-5'>
+                           Don’t miss amazing
+                           <br />
+                           grocery deals
+                        </h2>
+                        <p className='text-3xl text-gray-500 font-medium mb-12'>
+                           Save up to 50% off on your first order
+                        </p>
+                     </div>
                   </div>
-               </div>
-               <div>
-                  <img src={sliderImage2} alt='slider' className='w-full' />
-                  <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
-                     <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                        Fresh Vegetables
-                        <br />
-                        Big discount
-                     </h2>
-                     <p className='text-3xl text-gray-500 font-medium mb-12'>
-                        Save up to 50% off on your first order
-                     </p>
+                  <div>
+                     <img src={sliderImage2} alt='slider' className='w-full' />
+                     <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
+                        <h2 className='text-7xl text-gray-800 font-bold mb-5'>
+                           Fresh Vegetables
+                           <br />
+                           Big discount
+                        </h2>
+                        <p className='text-3xl text-gray-500 font-medium mb-12'>
+                           Save up to 50% off on your first order
+                        </p>
+                     </div>
                   </div>
-               </div>
-            </Carousel>
+               </Carousel>
+            </Transition>
          </div>
       </section>
    );
