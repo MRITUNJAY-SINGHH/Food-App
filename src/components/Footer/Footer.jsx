3ee64df794772626abef0ffae96e755e87f5e1c6
MRITUNJAY-SINGHH <mritunjays447@gmail.com>
2024-01-17 23:07:31 +0530

    Add newsletter section to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link } from 'react-router-dom';
 import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
+import banner from '../../assets/banner-9.png';
+// import icon1 from '../../assets/icon-1.png';
+// import icon2 from '../../assets/icon-2.png';
+// import icon3 from '../../assets/icon-3.png';
+// import icon4 from '../../assets/icon-4.png';
+// import icon5 from '../../assets/icon-5.svg';
+import './Footer.css';
+import { FaEnvelope } from 'react-icons/fa6';
 
 const Footer = () => {
    return (
       <>
          <footer className='py-3 dark:bg-gray-900'>
+            <div className='relative newsletter-inner flex justify-between items-center mb-10'>
+               <div className='ml-16'>
+                  <div className='flex flex-col'>
+                     <h2 className='text-[#253D4E] dark:text-white text-[45px] mb-4 font-bold'>
+                        Stay home & get your daily
+                     </h2>
+
+                     <h2 className='text-[#253D4E] dark:text-white text-[45px] mb-4  font-bold'>
+                        needs from our shop
+                     </h2>
+                  </div>
+
+                  <div>
+                     <h3 className='text-[#7E7E7E] text-[22px] font-normal '>
+                        Start You'r Daily Shopping with{' '}
+                        <span className='text-[#29A56C]'>Our Mart</span>
+                     </h3>
+                  </div>
+                  <div className='flex items-center gap-5 mt-10 outline-none border-none'>
+                     <input
+                        type='email'
+                        name='email'
+                        id='email'
+                        placeholder='Subscribe for latest updates'
+                        className='px-8 py-3 rounded-[25px] border-2 border-gray-300 focus:outline-none focus:border-indigo-500 dark:text-black dark:placeholder:text-black'
+                     />
+                     <button className='flex items-center gap-2 bg-indigo-500 text-white px-4 py-2 rounded-[25px] hover:bg-indigo-700 transition-colors duration-200'>
+                        <FaEnvelope />
+                        Subscribe
+                     </button>
+                  </div>
+               </div>
+               <div className='banner-9'>
+                  <img src={banner} alt='' />
+               </div>
+            </div>
             <div className='container-xxl'>
                <div className='flex justify-evenly items-center '>
                   <div>
