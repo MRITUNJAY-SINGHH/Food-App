3730436214aadc31d6154ed963f4dc3ce7ad8c45
MRITUNJAY-SINGHH <mritunjays447@gmail.com>
2024-01-18 22:38:35 +0530

    fix code

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -37,7 +37,7 @@ export default function HomeSlider() {
                autoPlaySpeed={3000}
                transitionDuration={500}
                // removeArrowOnDeviceType={['tablet', 'mobile']}
-               customButtonGroup={<></>} // This is to remove default button groups
+               customButtonGroup={<></>} // This is to remove default button
                customLeftArrow={<CustomLeftArrow />}
                customRightArrow={<CustomRightArrow />}
                dotListClass='custom-dot-list'
