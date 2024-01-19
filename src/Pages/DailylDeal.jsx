98b0913f177e5d3a9e2878367fb774ef06b40192
MRITUNJAY-SINGHH <mritunjays447@gmail.com>
2024-01-19 23:25:01 +0530

    Refactor DailyDeal component and update product data

--- src/Pages/DailylDeal.jsx
+++ src/Pages/DailylDeal.jsx
@@ -1,78 +1,154 @@
 /* eslint-disable react/prop-types */
-import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
 import 'react-multi-carousel/lib/styles.css';
-import Card from '../components/Card/Card';
+import Carousel from 'react-multi-carousel';
+
 import { FaTruckArrowRight } from 'react-icons/fa6';
+import banner4 from '../assets/banner-4.png';
+import DailyDealCard from '../components/Card/DailyDealCard';
 import product from '../assets/product-1-1.jpg';
 import product1 from '../assets/product-1-2.jpg';
 import productTwo from '../assets/product-2-1.jpg';
 import product2 from '../assets/product-2-2.jpg';
 import productThree from '../assets/product-3-1.jpg';
 import product3 from '../assets/product-3-2.jpg';
-// import productFour from '../assets/product-4-1.jpg';
-// import product4 from '../assets/product-4-2.jpg';
-// import productFive from '../assets/product-5-1.jpg';
-// import product5 from '../assets/product-5-2.jpg';
-// import productSix from '../assets/product-6-1.jpg';
-// import product6 from '../assets/product-6-2.jpg';
-// import productSeven from '../assets/product-7-1.jpg';
-// import product7 from '../assets/product-7-2.jpg';
-// import productEight from '../assets/product-8-1.jpg';
-// import product8 from '../assets/product-8-2.jpg';
-// import productNine from '../assets/product-9-1.jpg';
-// import product9 from '../assets/product-9-2.jpg';
-// import productTen from '../assets/product-10-1.jpg';
-// import product10 from '../assets/product-10-2.jpg';
-import banner4 from '../assets/banner-4.png';
-import Carousel from 'react-multi-carousel';
-
-const DailyDealData = [
-   {
-      id: 1,
-      name: 'Snakes',
-      title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
-      price: '$5.99',
-      discount: '$6.99',
-      rating: '4.5',
-      offer: 'Hot',
-      image: product,
-      image2: product1,
-   },
-   {
-      id: 2,
-      name: 'Hodo Foods',
-      title: 'All Natural Italian-Style Chicken Meatballs',
-      price: '$25.99',
-      discount: '$16.99',
-      rating: '4.5',
-      offer: 'Sale',
-      image: productTwo,
-      image2: product2,
-   },
-   {
-      id: 3,
-      name: 'Snakes',
-      title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
-      price: '$55.99',
-      discount: '$26.99',
-      rating: '4.0',
-      offer: 'Special',
-      image: productThree,
-      image2: product3,
-   },
-];
+import productFour from '../assets/product-4-1.jpg';
+import product4 from '../assets/product-4-2.jpg';
+import productFive from '../assets/product-5-1.jpg';
+import product5 from '../assets/product-5-2.jpg';
+import productSix from '../assets/product-6-1.jpg';
+import product6 from '../assets/product-6-2.jpg';
+import productSeven from '../assets/product-7-1.jpg';
+import product7 from '../assets/product-7-2.jpg';
+import productEight from '../assets/product-8-1.jpg';
+import product8 from '../assets/product-8-2.jpg';
+import productNine from '../assets/product-9-1.jpg';
+import product9 from '../assets/product-9-2.jpg';
+import productTen from '../assets/product-10-1.jpg';
+import product10 from '../assets/product-10-2.jpg';
 
 const DailyDeal = () => {
+   const productData = [
+      {
+         id: 1,
+         name: 'Snakes',
+         title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+         price: '$5.99',
+         discount: '$6.99',
+         rating: '4.5',
+         offer: 'Hot',
+         image: product,
+         image2: product1,
+      },
+      {
+         id: 2,
+         name: 'Hodo Foods',
+         title: 'All Natural Italian-Style Chicken Meatballs',
+         price: '$25.99',
+         discount: '$16.99',
+         rating: '4.5',
+         offer: 'Sale',
+         image: productTwo,
+         image2: product2,
+      },
+      {
+         id: 3,
+         name: 'Snakes',
+         title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
+         price: '$55.99',
+         discount: '$26.99',
+         rating: '4.0',
+         offer: 'Special',
+         image: productThree,
+         image2: product3,
+      },
+      {
+         id: 4,
+         name: 'Vegetables',
+         title: 'Foster Farms Takeout Crispy Classic Buffalo Wings',
+         price: '$50.99',
+         discount: '$30.99',
+         rating: '3.8',
+         offer: '10% Off',
+         image: productFour,
+         image2: product4,
+      },
+      {
+         id: 5,
+         name: 'Vegetables',
+         title: 'Blue Diamond Almonds Lightly Salted Vegetables',
+         price: '$20.99',
+         discount: '$10.99',
+         rating: '3.5',
+         offer: '-14% Off',
+         image: productFive,
+         image2: product5,
+      },
+      {
+         id: 6,
+         name: 'Hodo Foods',
+         title: 'Chobani Complete Vanilla Greek Yogurt',
+         price: '$25.99',
+         discount: '$15.99',
+         rating: '4.0',
+         offer: '-10% Off',
+         image: productSix,
+         image2: product6,
+      },
+      {
+         id: 7,
+         name: 'Meats',
+         title: 'Canada Dry Ginger Ale – 2 L Bottle - 200ml - 400g',
+         price: '$20.99',
+         discount: '$9.99',
+         rating: '4.3',
+         offer: '-10% Off',
+         image: productSeven,
+         image2: product7,
+      },
+      {
+         id: 8,
+         name: 'Cream',
+         title: 'Encore Seafoods Stuffed Alaskan Salmon',
+         price: '$25.99',
+         discount: '$19.99',
+         rating: '4.1',
+         offer: 'Hot',
+         image: productEight,
+         image2: product8,
+      },
+      {
+         id: 10,
+         name: 'Meats',
+         title: 'Gorton’s Beer Battered Fish Fillets with soft paper',
+         price: '$6.99',
+         discount: '$4.0',
+         rating: '4.0',
+         offer: 'New',
+         image: productNine,
+         image2: product9,
+      },
+      {
+         id: 9,
+         name: 'Home & Cleaning',
+         title: 'Haagen-Dazs Caramel Cone Ice Cream Ketchup',
+         price: '$16.99',
+         discount: '$8.0',
+         rating: '5.0',
+         offer: 'Hot-New',
+         image: productTen,
+         image2: product10,
+      },
+   ];
    const responsive = {
       desktop: {
          breakpoint: { max: 3000, min: 1024 },
          items: 4,
-         slidesToSlide: 1, // optional, default to 1.
+         slidesToSlide: 4, // optional, default to 1.
       },
       tablet: {
          breakpoint: { max: 1024, min: 464 },
          items: 2,
-         slidesToSlide: 1, // optional, default to 1.
+         slidesToSlide: 2, // optional, default to 1.
       },
       mobile: {
          breakpoint: { max: 464, min: 0 },
@@ -80,25 +156,6 @@ const DailyDeal = () => {
          slidesToSlide: 1, // optional, default to 1.
       },
    };
-   const CustomLeftArrow = ({ onClick }) => (
-      <button
-         type='button'
-         onClick={onClick}
-         className='custom-arrow custom-prev absolute rounded-full'
-      >
-         <IoIosArrowRoundBack size={40} />
-      </button>
-   );
-
-   const CustomRightArrow = ({ onClick }) => (
-      <button
-         type='button'
-         onClick={onClick}
-         className='custom-arrow custom-next absolute rounded-full'
-      >
-         <IoIosArrowRoundForward size={40} />
-      </button>
-   );
    return (
       <div className='p-6  banner-img '>
          <div className='px-6 pb-10'>
@@ -127,29 +184,14 @@ const DailyDeal = () => {
                   </div>
                </div>
             </div>
-            <div className='w-[80%]'>
-               {' '}
-               <div className=''>
-                  <Carousel
-                     responsive={responsive}
-                     showDots={true}
-                     infinite
-                     autoPlay
-                     autoPlaySpeed={3000}
-                     transitionDuration={500}
-                     customButtonGroup={<></>}
-                     customLeftArrow={<CustomLeftArrow />}
-                     customRightArrow={<CustomRightArrow />}
-                     dotListClass='custom-dot-list'
-                  >
-                     {DailyDealData.map((product, id) => (
-                        <div className='px-4 pb-12' key={id}>
-                           <Card key={product.id} productData={product} />
-                        </div>
-                     ))}
-                  </Carousel>
-               </div>
-            </div>
+            <Carousel responsive={responsive}>
+               {productData.map((productData) => (
+                  <DailyDealCard
+                     key={productData.id}
+                     productData={productData}
+                  />
+               ))}
+            </Carousel>
          </div>
       </div>
    );
---
+++ src/components/Card/DailyDealCard.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import Rating from '@mui/material/Rating';
+import { AiOutlineHeart, AiOutlineSwap, AiOutlineEye } from 'react-icons/ai';
+import { FaTruckArrowRight } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+
+const DailyDealCard = ({ productData }) => {
+   const [currentImage, setCurrentImage] = useState(productData.image);
+   const [hovered, setHovered] = useState(false);
+
+   const handleMouseMove = (event) => {
+      if (!hovered) {
+         setHovered(true);
+         const mouseX = event.nativeEvent.offsetX;
+         const cardWidth = event.target.clientWidth;
+         const ratio = mouseX / cardWidth;
+         const interpolatedImage =
+            ratio < 0.5 ? productData.image : productData.image2;
+         setCurrentImage(interpolatedImage);
+      }
+   };
+
+   const handleMouseLeave = () => {
+      setHovered(false);
+      setCurrentImage(productData.image);
+   };
+
+   return (
+      <div
+         className='p-6 w-full cardDesign relative'
+         onMouseMove={handleMouseMove}
+         onMouseLeave={handleMouseLeave}
+      >
+         <span className='badge absolute top-0 left-0 z-[99] text-white'>
+            {productData.offer}
+         </span>
+         <div className='imgWrapper relative'>
+            <img src={currentImage} alt='product' className='w-full' />
+            <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
+               <ul className='bg-white w-[200px] h-auto py-2 px-2 border border-solid border-[#BCE3C9] flex justify-between items-center product_list'>
+                  <Link tooltip='Add to Wishlist'>
+                     <li className='product_border'>
+                        <AiOutlineHeart size={25} />
+                     </li>
+                  </Link>
+                  <Link tooltip='Compare'>
+                     <li className='product_border'>
+                        <AiOutlineSwap size={25} />
+                     </li>
+                  </Link>
+                  <Link tooltip='Quick View'>
+                     <li className='product_border'>
+                        <AiOutlineEye size={25} />
+                     </li>
+                  </Link>
+               </ul>
+            </div>
+         </div>
+
+         <div className='productInfo'>
+            <span className='opacity-50 text-black text-sm dark:text-white dark:opacity-100'>
+               {productData.name}
+            </span>
+            <p className='text-[18px] w-[260px] font-semibold my-2 dark:text-white dark:opacity-100'>
+               {productData.title}
+            </p>
+            <div className='flex items-center text-sm my-2 dark:text-white dark:opacity-100'>
+               <Rating
+                  name='half-rating-read'
+                  defaultValue={parseFloat(productData.rating)}
+                  precision={0.5}
+                  readOnly
+                  className='rating'
+               />
+               <div>
+                  <span className='text-[#253D4E] ml-2 dark:text-white dark:opacity-100'>
+                     {productData.rating}
+                  </span>
+               </div>
+            </div>
+            <div className='flex justify-between items-center dark:text-white dark:opacity-100'>
+               <div className='flex  justify-center items-center'>
+                  <p className='text-[#3BB77E] font-bold text-[16px] dark:text-white'>
+                     {productData.price}
+                  </p>
+                  <p className='text-[#253D4E] ml-2 line-through text-[12px] dark:text-white'>
+                     {productData.discount}
+                  </p>
+               </div>
+               <div>
+                  <button className='text-white product-btn flex justify-center items-center '>
+                     Shop Now
+                     <FaTruckArrowRight className='inline-block ml-2 icon animate-icon' />
+                  </button>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default DailyDealCard;
